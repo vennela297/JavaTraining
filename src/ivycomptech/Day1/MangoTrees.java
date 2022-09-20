afef06cf051766686e3c09d2e6bc964a198aa0a9
vennela297 <vennuv000@gmail.com>
2022-09-20 23:13:17 +0530

    updated Mangofile

--- src/ivycomptech/Day1/MangoTrees.java
+++ src/ivycomptech/Day1/MangoTrees.java
@@ -1,5 +1,16 @@
 package ivycomptech.Day1;
 
+import java.util.Scanner;
+
 public class MangoTrees {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt(); int num = sc.nextInt();
+        if (num < col || num % col == 0 || num % col == 1)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
 
 }
